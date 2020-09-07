1cbd640df689fbe3c8da98795e3740c3e319d02d
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-09-07 14:57:32 +0500

    same change is

--- webrtc_videoCall/App_Start/RouteConfig.cs
+++ webrtc_videoCall/App_Start/RouteConfig.cs
@@ -12,12 +12,20 @@ namespace webrtc_videoCall
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            try
+            {
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                routes.MapRoute(
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+            }
+            catch (Exception)
+            {
+
+                
+            }
         }
     }
 }
